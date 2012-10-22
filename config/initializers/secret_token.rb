70faff089c2766ec81eee583d5ea12071cbe8ed6
Kevin Musiorski <musiorski@gmail.com>
2012-10-22 16:33:36 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyClub::Application.config.secret_token = 'd393bc8a1d909bea4df2347510741d9a7bb44b5beb9d81cb7a74db8491244dd897e4c241c6f72470329b4d44c8fff2e3bfaf891bfcabafe6eeabcba32903686e'
